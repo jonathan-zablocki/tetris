54a8579c116751c002d0a7dcb841ae35e70ccd28
jonathan zablocki <jonathan.z1996@gmail.com>
2020-07-05 15:25:17 -0400

    Updates

---
+++ precache-manifest.d8a0868616908eced441e13bfc85c74b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf4be4face4bd05abdce057d719c6c9",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "171138901c3bafb7a435",
+    "url": "/tetris/static/css/main.8a702a67.chunk.css"
+  },
+  {
+    "revision": "e84329b1ca191e943eca",
+    "url": "/tetris/static/js/2.aa48af83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tetris/static/js/2.aa48af83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171138901c3bafb7a435",
+    "url": "/tetris/static/js/main.c9d55f22.chunk.js"
+  },
+  {
+    "revision": "9d0f2c2c35f3704d579c",
+    "url": "/tetris/static/js/runtime-main.401af29c.js"
+  },
+  {
+    "revision": "3b372e9818fa4bfa9722ef07e774ee8f",
+    "url": "/tetris/static/media/Azonix-1VB0.3b372e98.otf"
+  },
+  {
+    "revision": "634fb91b3d2eb730fb7fef6548507be5",
+    "url": "/tetris/static/media/background.634fb91b.jpg"
+  }
+]);
